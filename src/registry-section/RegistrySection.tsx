9e375447dd4fc190aacea9147ff42b1a5462e240
Matt Newbigging <mrnewbigging@gmail.com>
2023-06-12 22:25:41 +0100

    registry section added

--- src/registry-section/RegistrySection.tsx
+++ src/registry-section/RegistrySection.tsx
@@ -14,15 +14,35 @@ export const RegistrySection: React.FC = () => {
       <div className='image-filter'></div>
       <div className='text-container'>
         <div className='background-filter'></div>
-        <div>
+        <div className='text-wrapper'>
           <div className='type-title'>Registry</div>
-          {/*<div className='type-text'>
+          <div className='type-text'>
             The best present you could possibly give us is the celebration of our marriage.
           </div>
           <div className='type-text'>
-            However, if you'd still like to spoil us, we've picked out a
-  </div>*/}
-          <div className='type-text'>Section still under construction...</div>
+            However, if you'd still like to spoil us, please keep reading.
+          </div>
+          <div className='type-text'>
+            We know that traditionally couples usually wish for items to help set up their new home
+            together, but having lived under the same roof for about six years we already have what
+            we need - and probably even more than we need!
+          </div>
+          <div className='type-text'>
+            Our next goal is to buy a house of our own in the country with a little garden for our
+            dog Lexie to play in. So, instead of asking for more stuff, we would be very grateful
+            for any donations to our house fund.
+          </div>
+          <div className='type-text'>
+            Donations can be made either directly to our bank account (please contact Matt or Elsa
+            for our bank details) or via our
+          </div>
+          <a
+            className='registry-button'
+            href='https://prezola.com/wishlists/10274537/'
+            target='_blank'
+          >
+            online registry
+          </a>
         </div>
       </div>
     </div>
